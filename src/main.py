7310b35c244398c1c96c4716d1cbd7879a8bc01b
Viktor Vesely <30593536+viktorvesely@users.noreply.github.com>
2020-01-20 00:47:55 +0100

    Changed environment

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ from DQN import *
 import os
 
 
-def make_video(env, TrainNet):
+def make_video(env, TrainNet, iterations):
     env = gym.wrappers.Monitor(env, os.path.join(os.getcwd(), "videos"), force=True)
     rewards = 0
     steps = 0
     done = False
     observation = env.reset()
-    while not done:
+    iter_per_episode = 50
+    while steps <= iterations:
         action = TrainNet.get_action(observation, 0)
         observation, reward, done, _ = env.step(action)
         steps += 1
@@ -29,19 +30,17 @@ def show_off(env, TrainNet):
         rewards += reward
     print("Testing steps: {} rewards {}: ".format(steps, rewards))
 
-def play_game(env, TrainNet, TargetNet, epsilon, copy_step):
+def play_game(env, TrainNet, TargetNet, epsilon, copy_step, iter_per_episode):
     rewards = 0
     iter = 0
     done = False
+    
     observations = env.reset()
-    while not done:
+    while iter <= iter_per_episode:
         action = TrainNet.get_action(observations, epsilon)
         prev_observations = observations
         observations, reward, done, _ = env.step(action)
         rewards += reward
-        if done:
-            reward = -200
-            env.reset()
 
         exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done}
         TrainNet.add_experience(exp)
@@ -51,34 +50,35 @@ def play_game(env, TrainNet, TargetNet, epsilon, copy_step):
             TargetNet.copy_weights(TrainNet)
     return rewards
 
+
 def main():
-    env = gym.make('CartPole-v0')
+    env = gym.make('Acrobot-v1')
     gamma = 0.99
     copy_step = 25
     num_states = len(env.observation_space.sample())
     num_actions = env.action_space.n
-    hidden_units = [32]
+    hidden_units = [64, 64]
     max_experiences = 10000
     min_experiences = 100
     batch_size = 32
+    iter_per_episode = 500
 
     TrainNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size)
     TargetNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size)
-    N = 10000
+    N = 100
     total_rewards = np.empty(N)
     epsilon = 0.99
     decay = 0.9999
     min_epsilon = 0.08
     for n in range(N):
         epsilon = max(min_epsilon, epsilon * decay)
-        total_reward = play_game(env, TrainNet, TargetNet, epsilon, copy_step)
+        total_reward = play_game(env, TrainNet, TargetNet, epsilon, copy_step, iter_per_episode)
         total_rewards[n] = total_reward
         avg_rewards = total_rewards[max(0, n - 100):(n + 1)].mean()
- 
-        if n % 100 == 0:
+        if n % 5 == 0:
             print("Progress:", int(n/N*100), "episode reward:", total_reward, "eps:", epsilon, "avg reward (last 100):", avg_rewards)
     print("avg reward for last 100 episodes:", avg_rewards)
-    make_video(env, TrainNet)
+    make_video(env, TrainNet, 1000)
     env.close()
 
 main()
