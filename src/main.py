a3feb0ac0668c603e26f7fc2bda9afd30d539b95
Viktor Vesely <30593536+viktorvesely@users.noreply.github.com>
2020-01-22 13:40:13 +0100

    Changed logic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import gym
 from DQN import *
 import os
-
+import keyboard as kb
 
 def make_video(env, TrainNet, iterations):
     env = gym.wrappers.Monitor(env, os.path.join(os.getcwd(), "videos"), force=True)
@@ -9,8 +9,9 @@ def make_video(env, TrainNet, iterations):
     steps = 0
     done = False
     observation = env.reset()
-    iter_per_episode = 50
+    print("start")
     while steps <= iterations:
+        print("step")
         action = TrainNet.get_action(observation, 0)
         observation, reward, done, _ = env.step(action)
         steps += 1
@@ -51,6 +52,18 @@ def play_game(env, TrainNet, TargetNet, epsilon, copy_step, iter_per_episode):
     return rewards
 
 
+def play():
+    env = gym.make("Acrobot-v1")
+    env.reset()
+    while not kb.is_pressed('q'):
+        env.render()
+        actions = 0
+        actions = 1 if kb.is_pressed('a') else actions
+        actions = 2 if kb.is_pressed('d') else actions
+        print(env.action_space.sample())
+        env.step(actions)
+    env.close()
+
 def main():
     env = gym.make('Acrobot-v1')
     gamma = 0.99
@@ -61,11 +74,11 @@ def main():
     max_experiences = 10000
     min_experiences = 100
     batch_size = 32
-    iter_per_episode = 500
+    iter_per_episode = 300
 
     TrainNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size)
     TargetNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size)
-    N = 100
+    N = 50
     total_rewards = np.empty(N)
     epsilon = 0.99
     decay = 0.9999
@@ -78,7 +91,7 @@ def main():
         if n % 5 == 0:
             print("Progress:", int(n/N*100), "episode reward:", total_reward, "eps:", epsilon, "avg reward (last 100):", avg_rewards)
     print("avg reward for last 100 episodes:", avg_rewards)
-    make_video(env, TrainNet, 1000)
+    make_video(env, TrainNet, 300)
     env.close()
 
-main()
+play()
